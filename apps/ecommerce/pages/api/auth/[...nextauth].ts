37cda999956f52aa9cfe13f559fd8c7ac0ad75d6
Sajib Hasan <sajib001@gmail.com>
2021-08-23 13:35:52 +0600

    Next auth issue

--- apps/ecommerce/pages/api/auth/[...nextauth].ts
+++ apps/ecommerce/pages/api/auth/[...nextauth].ts
@@ -12,8 +12,8 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     Providers.GitHub({
-      clientId: '1046561fd9ec9ee5f526',
-      clientSecret: '483facc0026a48f878c69f4c2b3a31d4a33c595f',
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
   adapter: PrismaAdapter(prisma),
